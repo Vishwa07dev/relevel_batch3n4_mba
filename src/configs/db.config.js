88d266101eaef0277758d1aa83afa3fc701626eb
veerabhoga <veerabhogavasantharayalu0@gmail.com>
2022-09-05 21:42:18 +0530

    task 2 has been completed

--- src/configs/db.config.js
+++ src/configs/db.config.js
@@ -1,5 +1,6 @@
 require('dotenv').config();
 
+
 module.exports = {
     DB_URL : process.env.DB_URL
 }
--- src/configs/server.config.js
+++ src/configs/server.config.js
@@ -2,7 +2,6 @@ if(process.env.NODE_ENV !== 'production'){
     require('dotenv').config();
 }
 
-
 module.exports = {
     PORT : process.env.PORT
 }
