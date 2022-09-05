f2a5a005792e1569b272a97d2a0dd4a47131632f
Dipankar <bhoumik.dipu@gmail.com>
2022-09-05 20:55:31 +0530

    complete task

--- routes/theatre.routes.js
+++ routes/theatre.routes.js
@@ -8,4 +8,15 @@
  *  GET /mba/api/v1/theatres
  * 
  *  DELETE /mba/api/v1/theatres/:id
- */
+*/
+
+
+const theatreController = require("../controllers/theatre.controller");
+
+module.exports = (app) => {
+  app.post("/mba/api/v1/theatres", theatreController.createTheatre);
+  app.put("/mba/api/v1/theatres/:id", theatreController.updateTheatre);
+  app.delete("/mba/api/v1/theatres/:id", theatreController.deleteTheatre);
+  app.get("/mba/api/v1/theatres", theatreController.findAllTheatre);
+  app.get("/mba/api/v1/theatres/:id", theatreController.findThreatreById);
+};
