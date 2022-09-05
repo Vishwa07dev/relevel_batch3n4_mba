46cbcded36e89cc9e58cc470120d3dceade2294d
Vishwa Mohan <vimohan@vimohan-mn2.linkedin.biz>
2022-09-05 20:23:50 +0530

    Task 2

---
+++ models/theatre.model.js
@@ -0,0 +1,32 @@
+const  mongoose = require("mongoose");
+
+const theatreSchema = new mongoose.Schema({
+
+    name : {
+        type : String,
+        required : true
+    },
+    description : {
+        type : String,
+        required : true
+    },
+    city : {
+        type : String,
+        required : true
+    },
+    pinCode : {
+        type : Number,
+        required : true
+    },
+    showTypes : {
+        type : String,
+        required : true
+        //TODO : MORNING | NOON | EVENING | NIGHT
+    },
+    numberOfSeats : {
+        type : Number,
+        required : true
+    }
+},{ timestamps : true , versionKey : false});
+
+module.exports = mongoose.model("Theatre" , theatreSchema);
