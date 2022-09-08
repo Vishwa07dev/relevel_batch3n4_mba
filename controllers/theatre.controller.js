7082aab81221526631ce9940b947a66af6939587
sayyedaaman2 <sayyedaaman9@gmail.com>
2022-09-08 22:34:51 +0530

    Task3 Completed

--- controllers/theatre.controller.js
+++ controllers/theatre.controller.js
@@ -37,7 +37,7 @@ exports.editTheatre = async (req,res)=>{
        theatre.showTypes = req.body.showTypes ? req.body.showTypes : theatre.showTypes,
        theatre.numberOfSeats = req.body.numberOfSeats ? req.body.numberOfSeats : theatre.numberOfSeats
 
-       const updatedTheatre = await Theatre.save();
+       const updatedTheatre = await theatre.save();
 
        console.log(`#### Theatre data updated ####`);
        res.status(200).send(updatedTheatre);
@@ -97,3 +97,50 @@ exports.getSingleTheatre = async (req,res)=>{
    }
 
 }
+
+
+exports.moviesInTheatre = async (req,res)=>{
+
+    try{
+        const theatre = await Theatre.findOne({_id : req.params.id}).populate('movies');
+        console.log(theatre);
+        res.status(200).send(theatre.movies);
+    
+    }catch(err){
+        console.log("#### Error while getting the theatre ####", err.message);
+        res.status(500).send({
+            message : "Internal server error while getting the theatre"
+        })
+    }
+ 
+ }
+
+ exports.moviesOnTheatres = async (req,res)=>{
+
+    try{
+        const theatre = await Theatre.findOne({_id : req.params.id});
+        
+        if(req.body.addMovies){
+            req.body.addMovies.forEach(movie => {
+                theatre.movies.push(movie)
+            })
+        }
+
+        if(req.body.removeMovies){
+            req.body.removeMovies.forEach(movie => {
+                theatre.movies.remove(movie)
+            })
+        }
+
+        await theatre.save();
+        res.status(201).send({message : "Updated movies in theatre"});
+
+    
+    }catch(err){
+        console.log("#### Error while performing movie on theatre ####", err.message);
+        res.status(500).send({
+            message : "Internal server error while getting the theatre"
+        })
+    }
+ 
+ }
--- routes/theatre.routes.js
+++ routes/theatre.routes.js
@@ -1,11 +1,15 @@
 const theatreController = require('../controllers/theatre.controller');
  
 module.exports = (app)=>{
-    app.post("/mbs/api/v1/theatres", theatreController.newTheatre);
-    app.put("/mbs/api/v1/theatres/:id", theatreController.editTheatre)
-    app.delete("/mbs/api/v1/theatres/:id", theatreController.deleteTheatre)
-    app.get("/mbs/api/v1/theatres", theatreController.getAllTheatres)
-    app.get("/mbs/api/v1/theatres/:id", theatreController.getSingleTheatre)
+    app.post("/mba/api/v1/theatres", theatreController.newTheatre);
+    app.put("/mba/api/v1/theatres/:id", theatreController.editTheatre)
+    app.delete("/mba/api/v1/theatres/:id", theatreController.deleteTheatre)
+    app.get("/mba/api/v1/theatres", theatreController.getAllTheatres)
+    app.get("/mba/api/v1/theatres/:id", theatreController.getSingleTheatre)
+    app.get("/mba/api/v1/theatres/:id/movies", theatreController.moviesInTheatre)
+    app.put("/mba/api/v1/theatres/:id/movies", theatreController.moviesOnTheatres)
+
+
 }
 
 /***
