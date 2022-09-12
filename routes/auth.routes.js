33bd9fa9afdb23357f167c84721daa5e73e5b991
MohammadJunaid <mojunaid2050@gmail.com>
2022-09-12 15:30:38 +0530

    code added

--- routes/auth.routes.js
+++ routes/auth.routes.js
@@ -1,6 +1,7 @@
 const authController = require('../controllers/auth.controller')
+const {verifySignUp} = require('../middlewares')
 
 module.exports = (app)=>{
-    app.post("/mba/api/v1/auth/signup", authController.signup)
-    app.post("/mba/api/v1/auth/signin", authController.signin)
+    app.post("/mba/api/v1/auth/signup", verifySignUp.validateSignupRequestBody, authController.signup)
+    app.post("/mba/api/v1/auth/signin", verifySignUp.validateSignInRequestBody, authController.signin)
 }
--- routes/theatre.routes.js
+++ routes/theatre.routes.js
@@ -3,12 +3,18 @@ const {authJwt} = require('../middlewares')
 
  
 module.exports = (app)=>{
+    
     app.post("/mba/api/v1/theatres", [authJwt.verifyToken, authJwt.isTheatreOwnerOrAdmin], theatreController.createNewTheatre);
+    
     app.put("/mba/api/v1/theatres/:id", [authJwt.verifyToken, authJwt.isTheatreOwnerOrAdmin, authJwt.isValidTheatreOwner], theatreController.editTheatre)
+    
     app.delete("/mba/api/v1/theatres/:id", [authJwt.verifyToken, authJwt.isTheatreOwnerOrAdmin, authJwt.isValidTheatreOwner], theatreController.deleteTheatre)
+    
     app.get("/mba/api/v1/theatres", [authJwt.verifyToken], theatreController.getAllTheatres)
+    
     app.get("/mba/api/v1/theatres/:id", [authJwt.verifyToken], theatreController.getSingleTheatre)
 
     app.get("/mba/api/v1/theatres/:id/movies", [authJwt.verifyToken], theatreController.getMoviesInTheatre)
+    
     app.put("/mba/api/v1/theatres/:id/movies", [authJwt.verifyToken, authJwt.isTheatreOwnerOrAdmin, authJwt.isValidTheatreOwner], theatreController.editMoviesInTheatre)
 }
