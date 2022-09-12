757adc49c3fd30d136a1b7fbe757085e27af2313
veerabhoga <veerabhogavasantharayalu0@gmail.com>
2022-09-12 17:58:37 +0530

    given task has been completed

--- routes/auth.routes.js
+++ routes/auth.routes.js
@@ -1,6 +1,7 @@
-const authController = require('../controllers/auth.controller')
+const authController = require('../controllers/auth.controller');
+const authJwt = require('../middlewares');
 
 module.exports = (app)=>{
-    app.post("/mba/api/v1/auth/signup", authController.signup)
-    app.post("/mba/api/v1/auth/signin", authController.signin)
+    app.post("/mba/api/v1/auth/signup",[authJwt.verifyToken], authController.signup);
+    app.post("/mba/api/v1/auth/signin",[authJwt.verifyToken], authController.signin);
 }
