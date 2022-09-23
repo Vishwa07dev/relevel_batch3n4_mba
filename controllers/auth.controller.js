f390ee1abe74c1434f891ad2e90f7b4cc5265370
LakshyaTecno <LakshyaTecno@gmail.com>
2022-09-23 22:47:56 +0530

    changes in  isTheatreOwnerOrAdmin

--- controllers/auth.controller.js
+++ controllers/auth.controller.js
@@ -57,17 +57,14 @@ exports.signin = async (req,res)=>{
         console.log(`#### ${user.userType} ${user.name} logged in ####`);
         const refreshToken = jwt.sign({id: user.userId}, authConfig.secret, {expiresIn : authConfig.JWT_TIME_REFRESH_TOKEN});
         
-         user.accessToken=accessToken,
-         user.refreshToken =refreshToken
-         await user.save()
         res.status(200).send({
             name : user.name,
             userId : user.userId,
             email : user.email,
             userType : user.userType,
             userStatus : user.userStatus,
-            accessToken :  user.accessToken,
-            refreshToken:  user.refreshToken
+            accessToken :  accessToken,
+            refreshToken:  refreshToken
         });
     }catch(err){
         console.log("#### Error while user sign in ##### ", err.message);
@@ -82,12 +79,10 @@ exports.signin = async (req,res)=>{
 exports.getAccessToken =async(req,res) =>{
     try{
 
-    console.log(req.user._id)
-    const user = await User.findOne({_id : req.user._id})
-    console.log(user)
+   
     const accessToken = jwt.sign({_id: req.user._id}, authConfig.secret, {expiresIn : authConfig.JWT_TIME_ACCESS_TOKEN});
-    user.accessToken = accessToken;
-    await user.save()
+    
+  
     res.status(200).send({accessToken:accessToken})    
     }catch(err){
         console.log("#### Error while user creating access token ##### ", err.message);
