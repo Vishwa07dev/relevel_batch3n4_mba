1ad10770810aaff931f2dad2a9b9ed764392e0a6
Cahndra <engtilakchandra@gmail.com>
2022-09-26 17:34:01 +0530

    Email notification

--- controllers/payment.controller.js
+++ controllers/payment.controller.js
@@ -1,18 +1,58 @@
 const Payment = require("../models/payment.model");
 const constants = require("../utils/constants");
+const clinetConfig = require('../configs/client.config');
+const sendNotificationReq = require('../utils/notificationClient');
 
-exports.getAllPayments = async (req, res) => {
-    try{
+module.exports = (subject, content, recepients, requester) => {
+
+    //create the request body
+    const reqBody = {
+        subject: subject,
+        recepientEmails: recepients,
+        content: content,
+        requester: requester
+    }
+
+
+    //Prepare the headers
+    const reqHeader = {
+        "Content-Type": "application/json"
+    }
+
+    //Combine headers and req body together
+    const args = {
+        data: reqBody,
+        headers: reqHeader
+    }
+    console.log(args);
+
+    //make a post call and handle the response 
+
+    try {
+        console.log("in the client function")
+        client.post(ClientRestCall.CLIENT_REST_CALL, args, (data, res) => {
+
+            console.log("Request sent");
+            console.log(data);
+
+        })
+    } catch (err) {
+        console.log("Some Error while sending the message : ", err.message);
+    }
+}
+
+exports.getAllPayments = async(req, res) => {
+    try {
         let queryObj = {};
 
-        if(req.user.userType != constants.userTypes.admin){
-           queryObj._id = req.user.myPayments;
+        if (req.user.userType != constants.userTypes.admin) {
+            queryObj._id = req.user.myPayments;
         }
-    
+
         const payments = await Payment.find(queryObj);
-    
+
         res.status(200).send(payments);
-    }catch{
+    } catch {
         console.log("Error while getting given all payment records", err.message);
 
         return res.status(500).send({
@@ -22,19 +62,19 @@ exports.getAllPayments = async (req, res) => {
 }
 
 
-exports.getOnePayment = async (req, res) => {
+exports.getOnePayment = async(req, res) => {
 
     res.status(200).send(req.paymentInParams);
 
 }
 
- exports.createPayment = async (req, res) => {
+exports.createPayment = async(req, res) => {
 
     try {
         const paymentObj = {
             bookingId: req.body.bookingId,
             amount: req.body.amount,
-            status: req.body.status           
+            status: req.body.status
         }
 
         const payment = await Payment.create(paymentObj);
@@ -44,10 +84,10 @@ exports.getOnePayment = async (req, res) => {
 
         req.user.myPayments.push(payment._id);
         await req.user.save();
-
+        await sendNotificationReq("Payment Successfuly MovieBooking", payment, req.user.email, clinetConfig.companyEmail);
         res.status(201).send(payment);
 
-    }catch (err) {
+    } catch (err) {
         console.log("Error while creating the payment", err.message);
 
         return res.status(500).send({
